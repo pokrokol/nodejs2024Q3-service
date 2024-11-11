c45aa1db8e3ecf0e13703631a57c87d0e5840725
ltldg <your_email@example.com>
2024-11-11 11:31:23 +0100

    add nest resources for project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistModule } from './artist/artist.module';
+import { UserModule } from './user/user.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [ArtistModule, UserModule, TrackModule, AlbumModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
